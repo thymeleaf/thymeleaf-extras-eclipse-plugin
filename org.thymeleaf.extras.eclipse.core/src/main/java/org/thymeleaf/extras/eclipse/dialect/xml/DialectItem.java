c4a44a2ecbea75653ccceb18cf6660ab4fbf5b8a
Emanuel Rabina <emanuelrabina@gmail.com>
2021-09-24 20:54:44 +1200

    Update jaxb-utilities to work w/ Java 17

--- org.thymeleaf.extras.eclipse.core/src/main/java/org/thymeleaf/extras/eclipse/dialect/xml/DialectItem.java
+++ org.thymeleaf.extras.eclipse.core/src/main/java/org/thymeleaf/extras/eclipse/dialect/xml/DialectItem.java
@@ -8,11 +8,12 @@
 
 package org.thymeleaf.extras.eclipse.dialect.xml;
 
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlAttribute;
-import javax.xml.bind.annotation.XmlSeeAlso;
-import javax.xml.bind.annotation.XmlType;
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlAttribute;
+import jakarta.xml.bind.annotation.XmlSeeAlso;
+import jakarta.xml.bind.annotation.XmlTransient;
+import jakarta.xml.bind.annotation.XmlType;
 
 
 /**
@@ -137,7 +138,7 @@ public abstract class DialectItem {
         return (this.clazz!= null);
     }
     
-	@javax.xml.bind.annotation.XmlTransient
+	@XmlTransient
 	protected Dialect dialect;
 
 	/**
